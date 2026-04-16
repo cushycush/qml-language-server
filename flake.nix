32ae7e8c99777724a2d5deb606d87c37badfe884
Matthew Cushing <hgxtymphwn@privaterelay.appleid.com>
2026-04-16 13:48:10 -0600

    feat: add Nix flake and CI to auto-compute vendorHash (#9)
    Adds flake.nix with buildGoModule for nix run/install. A CI workflow
    triggers on go.mod/go.sum changes to compute the correct vendorHash
    and commit it back. README updated with Nix installation instructions.

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Language Server Protocol implementation for QML";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        version = if self ? shortRev then "0.0.0-${self.shortRev}" else "dev";
+      in
+      {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "qml-language-server";
+            inherit version;
+            src = self;
+
+            vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+
+            ldflags = [
+              "-s" "-w"
+              "-X main.version=${version}"
+            ];
+
+            meta = with pkgs.lib; {
+              description = "Language Server Protocol implementation for QML";
+              homepage = "https://github.com/cushycush/qml-language-server";
+              license = licenses.mit;
+              maintainers = [ ];
+              mainProgram = "qml-language-server";
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            golangci-lint
+          ];
+        };
+      }
+    );
+}
